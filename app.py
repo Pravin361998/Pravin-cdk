7e66df9c60764f22037f5f4085995b2d2a42b4ff
Pravin361998 <111245187+Pravin361998@users.noreply.github.com>
2023-01-03 11:17:36 +0530

    New s3 updated

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import aws_cdk as cdk
 from stacks.vpc_stack import VPCStack
 from stacks.security_group import SGStack
 from stacks.s3_stack import S3Stack
+from stacks.iam_stack import IAM
 
 
 app = cdk.App()
-vpc = VPCStack(app, "VPCStack",
-)
-#sg = SGStack(app, "security", vpc=vpc.vpc)
+vpc = VPCStack(app, "VPCStack",)
+#IAM(app, "IAM",)
 S3Stack(app, "S3Stack", )
 
 app.synth()
--- stacks/s3_stack.py
+++ stacks/s3_stack.py
@@ -1,4 +1,4 @@
-from aws_cdk import  aws_s3, Stack
+from aws_cdk import  aws_s3, Stack, Duration
 from constructs import Construct
 
 class S3Stack(Stack):
@@ -8,6 +8,28 @@ class S3Stack(Stack):
 
         aws_s3.Bucket(
             self,
-            'qwedsazxcde',
-            bucket_name = 'qwedsazxcde'
+            'pravin',
+            bucket_name = 'pravinnewtest023',
+            block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
+            versioned=True,
+            lifecycle_rules=[
+           aws_s3.LifecycleRule(
+          enabled=True,
+          expiration=Duration.days(365),
+          transitions=[
+            aws_s3.Transition(
+              storage_class=aws_s3.StorageClass.INFREQUENT_ACCESS,
+              transition_after=Duration.days(30)
+            ),
+            aws_s3.Transition(
+              storage_class=aws_s3.StorageClass.GLACIER,
+              transition_after=Duration.days(90)
+            ),
+          ]
+          
+           )
+            ]
+            
+
+        
         )
